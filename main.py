1712abb0f7332b54fa0ae36d378d8bd7f6962490
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-29 13:05:16 +0800

    kumite

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
-app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 app.run(debug=True,port=5001)
 
