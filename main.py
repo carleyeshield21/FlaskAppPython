2e88ac34f7cd0dc75a2445a7fa507edff805b35c
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-29 12:23:42 +0800

    kumite

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+app.run(debug=True,port=5001)
+
+
+
+
+
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+    # print_hi('PyCharm')
