37cd06239e14b60b02ea0fc384ebec39e9746026
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-30 14:40:00 +0800

    kumite

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__,template_folder='template')
 
 app.config['SECRET KEY'] = 'ang_sikreto_ay_mananatiling_lihim' #specify the parameters of the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #specify the parameters of the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #specify the parameters of the database, running the program will automatically create
+# a new folder directory instance with a data.db file
 db = SQLAlchemy(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True) #as an indentifier of each row of the table
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -22,13 +31,7 @@ def index():
 
     return render_template("index.html")
 
-app.run(debug=True,port=5001)
-
-
-
-
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-    # print_hi('PyCharm')
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True,port=5001)
