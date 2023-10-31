d235bec3bb1da71826352e1aea15717b60b62bc2
carleyeshield <carlo_yamzon@yahoo.com>
2023-10-31 14:12:00 +0800

    kumite

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = Flask(__name__,template_folder='template')
 app.config['SECRET_KEY'] = 'ang_sikreto_ay_mananatiling_lihim' #specify the parameters of the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #specify the parameters of the database, running the program will automatically create
 # a new folder directory instance with a data.db file
+app.config['MAIL_PASSWORD'] ='ejqkwgqsbylewfub
+'
 db = SQLAlchemy(app)
 
 class Form(db.Model): #this will create a table in which can be viewed in the sqlite app
